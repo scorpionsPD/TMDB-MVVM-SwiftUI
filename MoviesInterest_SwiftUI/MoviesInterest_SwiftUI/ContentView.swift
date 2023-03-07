55ee3b016b068c643bec7cc46d14b80151006c02
Pradeep Dahiya <pradeep.dahiya@crmnext.com>
2023-03-07 11:25:06 +0000

    Project Creation

---
+++ MoviesInterest_SwiftUI/MoviesInterest_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesInterest_SwiftUI
+//
+//  Created by Pradeep Dahiya on 07/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesInterest_SwiftUI/MoviesInterest_SwiftUI/MoviesInterest_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesInterest_SwiftUIApp.swift
+//  MoviesInterest_SwiftUI
+//
+//  Created by Pradeep Dahiya on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesInterest_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
